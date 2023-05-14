a4f38939b5d4b0e9d1bdfa36fe1a02ddf22af29d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-05-15 02:21:54 +0530

    Bus Reservation - Multithreading

--- OS/BusReservation.java
+++ OS/BusReservation.java
@@ -1,35 +1,34 @@
-public class BusReservation extends Thread{
-	int vacant=2,required;
-	BusReservation(int r){
-		required=r;
-	}
-	
-	public synchronized void run(){
-	
-	System.out.println("Welcome "+ Thread.currentThread().getName()+"!");
- 	System.out.println("No. of seats left: " + vacant);
-	if(required<=vacant){
-	
-	System.out.println(required + " Tickets have been booked");
-	try{
-		Thread.sleep(1000);
-	}catch(Exception e){
-		e.printStackTrace();
-	}
-	vacant=vacant-required;
-	}else{
-	System.out.println("All Tickets have been booked");
-	}
-	}
+public class BusReservation extends Thread {
 
-	public static void main (String arg[]){
-	BusReservation br=new BusReservation (2);
-	Thread t1=new Thread(br);
-	Thread t2=new Thread(br);
-	t1.setName("Akshay");
-	t2.setName("Suresh");
-	t1.start();
-	t2.start();
-	}
+  int vacant = 2, required;
 
+  BusReservation(int r) {
+    required = r;
+  }
+
+  public synchronized void run() {
+    System.out.println("Welcome " + Thread.currentThread().getName() + "!");
+    System.out.println("No. of seats left: " + vacant);
+    if (required <= vacant) {
+      System.out.println(required + " Tickets have been booked");
+      try {
+        Thread.sleep(1000);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      vacant = vacant - required;
+    } else {
+      System.out.println("All Tickets have been booked");
+    }
+  }
+
+  public static void main(String arg[]) {
+    BusReservation br = new BusReservation(2);
+    Thread t1 = new Thread(br);
+    Thread t2 = new Thread(br);
+    t1.setName("Akshay");
+    t2.setName("Suresh");
+    t1.start();
+    t2.start();
+  }
 }
