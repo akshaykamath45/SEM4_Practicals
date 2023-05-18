8a56e2a1f32441413211d830efb38515a893632c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-05-19 00:11:40 +0530

    Min Max Algorithm
    updated,better than previous.

--- AOA/3.1.c
+++ AOA/3.1.c
@@ -1,68 +1,53 @@
 #include <stdio.h>
-#define MAX 20
+#include<stdlib.h>
+int min,max;
 
-void min_max(int A[], int i, int j, int max[], int min[], int index, int *min_both, int *max_both)
-{
-    if (i == j)//Single Elements
-    {
-        min[index] = A[i];
-        max[index] = A[i];
-        return;
+void minMax(int arr[],int first,int last){
+    int mid,max1,min1;
+    if(first==last){
+        max=arr[first];
+        min=arr[first];
     }
-    else if (i == j - 1)//2 Elements
-    {
-        if (A[i] < A[j])
-        {
-            min[index] = A[i];
-            max[index] = A[j];
+    else if(first==last-1){
+        if(arr[first]>arr[last]){
+            max=arr[first];
+            min=arr[last];
+        }else{
+            max=arr[last];
+            min=arr[first];
         }
-        else
-        {
-            min[index] = A[j];
-            max[index] = A[i];
+    }else{
+        mid=(first+last)/2;
+        minMax(arr,first,mid);
+        max1=max;
+        min1=min;
+        minMax(arr,mid+1,last);
+        if(max<max1){
+            max=max1;
         }
-        return;
-    }
-    else // More than 2 elements
-    {
-        int mid = (i + j) / 2;
-
-        min_max(A, i, mid, max, min, 0, min_both, max_both); // Left subarray call
-
-        min_max(A, mid + 1, j, max, min, 1, min_both, max_both); // Right subarray call
-
-        if (max[0] < max[1] && *max_both < max[1])
-            *max_both = max[1];
-        else if (max[0] > max[1] && *max_both < max[0])
-            *max_both = max[0];
-        if (min[0] < min[1] && *min_both > min[0])
-            *min_both = min[0];
-        else if (min[0] > min[1] && *min_both > min[1])
-            *min_both = min[1];
+        if(min>min1){
+            min=min1;
+        }
     }
 }
-
 int main()
 {
-    int n, arr[MAX];
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter number %d: ", i + 1);
-        scanf("%d", &arr[i]);
+    int n,i;
+    printf("Enter size of array : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("The random array is : ");
+    for(i=0;i<n;i++){
+        arr[i]=rand()%1000+1;
+        printf("%d ",arr[i]);
     }
 
-    int max[2], min[2];
-    int i = 0, j = n - 1;
-    int minimum = arr[0], maximum = arr[0];
-
-    max[0] = arr[0], min[0] = arr[0], max[1] = arr[0], min[1] = arr[0];
-
-    min_max(arr, i, j, max, min, 0, &minimum, &maximum);
-
-    printf("Maximum: %d\n", maximum);
-    printf("Minimum: %d", minimum);
+    min=arr[0];
+    max=arr[0];
+    minMax(arr,0,n-1);
+    printf("\n");
+    
+    printf("Max= %d \nMin=%d\n",max,min);
 
     return 0;
 }
